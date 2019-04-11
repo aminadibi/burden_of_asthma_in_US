aecedc05d7ca31d2623dd58f9bc0046b38d9edf3
ahill187 <ainsleighhill@gmail.com>
2019-04-11 14:54:01 -0700

    Fixed QALY graph bug and Year slider bug

--- app.R
+++ app.R
@@ -149,7 +149,7 @@ server <- function(input, output, session) {
                         tabItemsList[[input$selectedTab]]$tabNumber
                     })
                     columnOptions = reactive({
-                        return(appData$tabs$columnOptions[[tabNumber()]])
+                        return(sapply(appData$tabs$columnOptions, "[[", tabNumber()))
                     })
                     sidebarIds = reactive({
                         tabItemsList[[tabNumber()]]$sidebarShownIds
@@ -162,12 +162,16 @@ server <- function(input, output, session) {
                             types = appData$tabs$columnTypes[[tabNumber()]]
                             ids = tabItemsList[[tabNumber()]]$sidebarShownIds
                             columnTypes = appData$tabs$columnTypes[[tabNumber()]]
+                            print(sidebarIds())
+                            print(columnOptions())
+                            print(types)
+                            print(tabNumber())
+                            print(tabItemsList[[tabNumber()]]$sidebarHiddenChoices[[index]])
                             updateOptions = getOptions(
                                 isolate(input), sidebarIds(),columnOptions(),
                                 types[index], index, types,
                                 tabItemsList[[tabNumber()]]$sidebarHiddenChoices[[index]],
                                 allValues[index])
-                            print(types[index])
                             print(updateOptions)
                             if(updateOptions!=FALSE){
                                 updateSelectInput(session, ids[index], label = labels[index],
@@ -176,8 +180,10 @@ server <- function(input, output, session) {
                     })
 
                     output[[googleChartId]] = renderGoogleChart({
-                        selectedTabItem = tabItemsList[[input$selectedTab]]
+                        selectedTabItem = tabItemsList[[tabNumber()]]
                         sidebarShownIds = selectedTabItem$sidebarShownIds
+                        print(sidebarShownIds)
+                        googleChartId = tabItemsList[[tabNumber()]]$googleChartOutputId
                         year = input[[sidebarShownIds[5]]]
                         xAxis = input[[sidebarShownIds[1]]]
                         yAxis = input[[sidebarShownIds[2]]]
@@ -194,10 +200,12 @@ server <- function(input, output, session) {
 
                         }
                         if (year == "All Years") {
-                            year = c(19, 20, 21, 22, 23, 24, 25)
+                            year = c(19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+                                     36, 37, 38)
                         } else {
                             year = as.numeric(year)
                         }
+                        print(googleChartId)
                         googleChart(
                             title = "",
                             elementId = googleChartId,
@@ -215,6 +223,10 @@ server <- function(input, output, session) {
                                         "25 to 29 years",
                                         "30 to 34 years",
                                         "35 to 39 years",
+                                        "40 to 44 years",
+                                        "45 to 49 years",
+                                        "50 to 54 years",
+                                        "55 to 59 years",
                                         "80 to 84 years"
                                     )
                                 ),
@@ -317,10 +329,16 @@ server <- function(input, output, session) {
                         leafletMap <- leafletMapList[[selectedTab]]
                         return(leafletMap)
                     })
-                    year <- 19
+                    tabNumber = reactive({
+                        tabItemsList[[input$selectedTab]]$tabNumber
+                    })
+                    year <- reactive({
+                        input[[tabItemsList[[tabNumber()]]$sliderId]] - 2000
+                    })
 
                     output[[mapOutputId]] <- renderLeaflet({
-                        leafletMap()$drawMap(year)
+                        print(year())
+                        leafletMap()$drawMap(year())
                     })
                     cat("~~~ Setting up Info Boxes ~~~", fill = T)
 
@@ -352,7 +370,7 @@ server <- function(input, output, session) {
                             regionId = as.numeric(layerRegionId[[1]][4])
                             value <-
                                 leafletMap()$getLayerValueData(valueName = valueName,
-                                                             year = 19,
+                                                             year = year(),
                                                              layer = layerId)
                             regionName = leafletMap()$regionNames[regionId]
                             subtitles = names(tabItemDash$valueBoxChoices)
